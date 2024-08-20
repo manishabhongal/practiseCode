0ae519fbf5d72141c2f490d7dad6227ffcdff4ff
manishabhongal <bhongalmanisha@gmail.com>
2024-08-20 20:41:57 +0530

    [Manisha] added main method in Cov_TC_456_validateAgeDropDown

--- src/javaCodeExample/Cov_TC_456_validateAgeDropDown.java
+++ src/javaCodeExample/Cov_TC_456_validateAgeDropDown.java
@@ -1,7 +1,10 @@
 package javaCodeExample;
 
 public class Cov_TC_456_validateAgeDropDown {
+	public static void main(String[] args) {
+		System.out.println("this is new modification");
+	}
 	
-	System.out.println("this is new modification");
+	
 
 }
